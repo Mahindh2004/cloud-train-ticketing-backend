8ace4b9726acc854c325db6b030e722c291c8f66
mahindh2004 <mahindhsuriyakumar2004@gmail.com>
2025-09-12 12:02:30 +0530

    commit

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize(
   process.env.DB_USER,     // trainuser
   process.env.DB_PASSWORD, // yourpassword
   {
-    host: process.env.DB_HOST || "localhost",
+    host: process.env.DB_HOST,
     dialect: "mysql",
     port: process.env.DB_PORT || 3306,
     logging: false
