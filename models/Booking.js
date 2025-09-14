539b7da638df617fffa4f5d40a76476ee7c94937
mahindh2004 <mahindhsuriyakumar2004@gmail.com>
2025-09-14 11:03:25 +0530

    commit

--- models/Booking.js
+++ models/Booking.js
@@ -1,17 +1,34 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../config/db");
-const User = require("./User");
 const Train = require("./Train");
 
 const Booking = sequelize.define("Booking", {
-  booking_id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-  status: { type: DataTypes.STRING, defaultValue: "Booked" }
+  booking_id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  train_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: Train,
+      key: "train_id",
+    },
+  },
+  travel_date: {
+    type: DataTypes.DATEONLY,
+    allowNull: false,
+  },
+  seats_available: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
 });
 
-User.hasMany(Booking, { foreignKey: "user_id" });
-Booking.belongsTo(User, { foreignKey: "user_id" });
-
+// associations
 Train.hasMany(Booking, { foreignKey: "train_id" });
 Booking.belongsTo(Train, { foreignKey: "train_id" });
 
 module.exports = Booking;
+
--- models/Train.js
+++ models/Train.js
@@ -2,13 +2,40 @@ const { DataTypes } = require("sequelize");
 const sequelize = require("../config/db");
 
 const Train = sequelize.define("Train", {
-  train_id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-  train_name: { type: DataTypes.STRING, allowNull: false },
-  source: { type: DataTypes.STRING, allowNull: false },
-  destination: { type: DataTypes.STRING, allowNull: false },
-  seats_available: { type: DataTypes.INTEGER, allowNull: false },
-  departure_time: { type: DataTypes.STRING, allowNull: false },
-  arrival_time: { type: DataTypes.STRING, allowNull: false }
+  train_id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  train_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  source: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  destination: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  departure_time: {
+    type: DataTypes.STRING, // or TIME if you prefer
+    allowNull: false,
+  },
+  arrival_time: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  base_fare: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  total_seats: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
 });
 
 module.exports = Train;
+
