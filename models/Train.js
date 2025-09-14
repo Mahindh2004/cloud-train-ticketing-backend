4fa5e0c438266b3933c63cfc6845570a2f268ce1
mahindh2004 <mahindhsuriyakumar2004@gmail.com>
2025-09-14 11:44:55 +0530

    commit

--- models/Train.js
+++ models/Train.js
@@ -1,41 +1,49 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../config/db");
 
-const Train = sequelize.define("Train", {
-  train_id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
+const Train = sequelize.define(
+  "Train", // model name
+  {
+    train_id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    train_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    source: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    destination: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    departure_time: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    arrival_time: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    base_fare: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    total_seats: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
   },
-  train_name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  source: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  destination: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  departure_time: {
-    type: DataTypes.STRING, // or TIME if you prefer
-    allowNull: false,
-  },
-  arrival_time: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  base_fare: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  total_seats: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-});
+  {
+    tableName: "Trains", // explicitly use your MySQL table
+    timestamps: false,   // remove createdAt/updatedAt
+  }
+);
 
 module.exports = Train;
+
 
